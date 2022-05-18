055fe1943026c8451aa06115b80cabbfe4caa29c
Ricardo Silva <cilipimania@gmail.com>
2022-05-18 09:05:48 -0300

    First commit course

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rick-morty.png';
+import {useState} from 'react';
 import './App.css';
+import Characters from './Components/Characters';
 
 function App() {
+  const restApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  }
+
+  const [characters, setCharacters] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title"> Rick & Morty</h1>
+        {characters ? (
+            <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home"/>
+            <button onClick={restApi} class="btn-search">Buscar Personajes</button>
+          </>
+        )}
       </header>
     </div>
   );
